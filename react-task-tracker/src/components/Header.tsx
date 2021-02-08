33ca41b1a6e6c477b9feb87f8793b3194732b9a2
nagaita <tn00gm@gmail.com>
2021-02-09 05:04:24 +0900

    Add button

--- react-task-tracker/src/components/Header.tsx
+++ react-task-tracker/src/components/Header.tsx
@@ -6,10 +6,9 @@ type Props = {
 
 const Header: React.FC<Props> = ({ title }) => {
     return (
-        <header>
-            <h1 >
-                {title}
-            </h1>
+        <header className='header'>
+            <h1 >{title}</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
