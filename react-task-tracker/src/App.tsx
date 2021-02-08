d05407469483ecacbae39a148533e83af56b8fb2
nagaita <tn00gm@gmail.com>
2021-02-08 13:24:48 +0900

    component argument

--- react-task-tracker/src/App.tsx
+++ react-task-tracker/src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header'
 function App() {
   return (
     <div className="container">
-      <Header />
+      <Header title="Task Tracker from argument"/>
     </div>
   );
 }
--- react-task-tracker/src/components/Header.tsx
+++ react-task-tracker/src/components/Header.tsx
@@ -1,7 +1,13 @@
-const Header = () => {
+import React from 'react'
+
+type Props = {
+    title: string
+}
+
+const Header: React.FC<Props> = (props) => {
     return (
         <header>
-            <h1>Task Tracker</h1>
+            <h1>{props.title}</h1>
         </header>
     )
 }
