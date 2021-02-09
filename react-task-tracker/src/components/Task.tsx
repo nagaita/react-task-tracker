85fee80cb9347d9383f8d0e9cd445c01daf98a54
nagaita <tn00gm@gmail.com>
2021-02-10 06:21:24 +0900

    show task.day

--- react-task-tracker/src/components/Task.tsx
+++ react-task-tracker/src/components/Task.tsx
@@ -15,6 +15,7 @@ const Task: React.FC<Props> = ({ task }) => {
     return (
         <div className='task'>
             <h3 key={task.id}>{task.text}</h3>
+            <p>{task.day}</p>
         </div>
     )
 }
