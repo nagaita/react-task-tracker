2b0614c79e0ba8758064ae676181796602ba3a32
nagaita <tn00gm@gmail.com>
2021-02-10 06:01:09 +0900

    useState

--- react-task-tracker/src/components/Tasks.tsx
+++ react-task-tracker/src/components/Tasks.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-
-const tasks = [
-    {
-        id: 1,
-        text: 'task 1',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'task 2',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'task 3',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true
-    },
-]
+import { useState } from 'react'
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'task 1',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'task 2',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'task 3',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true
+        },
+    ])
+
     return (
         <div>
             {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}
