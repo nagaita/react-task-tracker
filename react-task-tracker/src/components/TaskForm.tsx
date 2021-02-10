08c536af1c48338d07e5b44ae341ec1d11c18f04
nagaita <tn00gm@gmail.com>
2021-02-10 13:15:33 +0900

    state for task form

--- react-task-tracker/src/components/TaskForm.tsx
+++ react-task-tracker/src/components/TaskForm.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import { useState } from 'react'
 
 const TaskForm = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
     return (
         <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task' />
+                <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)} />
             </div>
             <div className='form-control'>
                 <label>Day</label>
-                <input type='text' placeholder='Add Day' />
+                <input type='text' placeholder='Add Day' value={day} onChange={(e) => setDay(e.target.value)} />
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' checked={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
             </div>
 
             <input type='submit' value='Save Task' className='btn btn-block'/>
